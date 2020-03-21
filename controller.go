13cc72c0a243661bdb41ca7934175ce18f88782a
SlothNinja <slothninja2@gmail.com>
2020-03-21 16:21:27 -0400

    migrated memcache

--- controller.go
+++ controller.go
@@ -12,6 +12,7 @@ import (
 	"github.com/SlothNinja/contest"
 	"github.com/SlothNinja/game"
 	"github.com/SlothNinja/log"
+	"github.com/SlothNinja/memcache"
 	"github.com/SlothNinja/mlog"
 	"github.com/SlothNinja/restful"
 	"github.com/SlothNinja/sn"
@@ -19,8 +20,6 @@ import (
 	"github.com/SlothNinja/user"
 	stats "github.com/SlothNinja/user-stats"
 	"github.com/gin-gonic/gin"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/memcache"
 )
 
 const (
@@ -173,7 +172,7 @@ func Update(prefix string) gin.HandlerFunc {
 			}
 		case actionType == game.Undo:
 			mkey := g.UndoKey(c)
-			if err := memcache.Delete(appengine.NewContext(c.Request), mkey); err != nil && err != memcache.ErrCacheMiss {
+			if err := memcache.Delete(c, mkey); err != nil && err != memcache.ErrCacheMiss {
 				log.Errorf("memcache.Delete error: %s", err)
 				c.Redirect(http.StatusSeeOther, showPath(prefix, c.Param(hParam)))
 				return
@@ -229,7 +228,7 @@ func (g *Game) save(c *gin.Context) error {
 			return err
 		}
 
-		err = memcache.Delete(appengine.NewContext(c.Request), g.UndoKey(c))
+		err = memcache.Delete(c, g.UndoKey(c))
 		if err == memcache.ErrCacheMiss {
 			return nil
 		}
@@ -268,7 +267,7 @@ func (g *Game) saveWith(c *gin.Context, ks []*datastore.Key, es []interface{}) e
 			return err
 		}
 
-		err = memcache.Delete(appengine.NewContext(c.Request), g.UndoKey(c))
+		err = memcache.Delete(c, g.UndoKey(c))
 		if err == memcache.ErrCacheMiss {
 			return nil
 		}
@@ -302,7 +301,7 @@ func (g *Game) cache(c *gin.Context) error {
 		return err
 	}
 	item.Value = v
-	return memcache.Set(appengine.NewContext(c.Request), item)
+	return memcache.Set(c, item)
 }
 
 func wrap(s *stats.Stats, cs contest.Contests) ([]*datastore.Key, []interface{}) {
@@ -346,7 +345,7 @@ func Undo(prefix string) gin.HandlerFunc {
 			return
 		}
 		mkey := g.UndoKey(c)
-		if err := memcache.Delete(appengine.NewContext(c.Request), mkey); err != nil && err != memcache.ErrCacheMiss {
+		if err := memcache.Delete(c, mkey); err != nil && err != memcache.ErrCacheMiss {
 			log.Errorf("Controller#Undo Error: %s", err)
 		}
 	}
@@ -564,7 +563,7 @@ func mcGet(c *gin.Context, g *Game) error {
 	defer log.Debugf("Exiting")
 
 	mkey := g.GetHeader().UndoKey(c)
-	item, err := memcache.Get(appengine.NewContext(c.Request), mkey)
+	item, err := memcache.Get(c, mkey)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/SlothNinja/contest v0.0.0-20200319032446-29b690351b81
 	github.com/SlothNinja/game v0.0.0-20200321040413-d51ae22f32e0
 	github.com/SlothNinja/log v0.0.2
+	github.com/SlothNinja/memcache v0.0.0-20200321195207-184761e76825
 	github.com/SlothNinja/mlog v0.0.0-20200319132402-ae728a349beb
+	github.com/SlothNinja/rating v0.0.0-20200321184620-3c544e573b3b // indirect
 	github.com/SlothNinja/restful v0.0.0-20200317231650-e1a2eb3f56b2
 	github.com/SlothNinja/send v0.0.0-20200321040208-716c0a7a4470
 	github.com/SlothNinja/sn v0.0.0-20200319230317-a899e08418dc
@@ -16,5 +18,4 @@ require (
 	github.com/SlothNinja/user-stats v0.0.0-20200316003629-dfe63e057543
 	github.com/gin-gonic/gin v1.5.0
 	github.com/mailjet/mailjet-apiv3-go v0.0.0-20190724151621-55e56f74078c
-	google.golang.org/appengine v1.6.5
 )
